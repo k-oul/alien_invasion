ef91568587c745fa181875a1f92999e42a9d8c94
K_oul <fine3kuandu@163.com>
2018-05-08 17:00:19 +0800

    保持最高积分
    保持最高积分

--- game_stats.py
+++ game_stats.py
@@ -5,11 +5,15 @@ class GameStats():
         self.ai_settings = ai_settings
         self.reset_stats()
         self.game_active = False
-        self.high_score = 0
+        with open("high_score.txt", "r") as h:
+            hi = h.read()
+        self.high_score = int(hi)
 
     def reset_stats(self):
         """初始化在游戏运行期间可能变化的统计信息"""
         self.ships_left = self.ai_settings.ship_limit
         self.score = 0
         self.level = 1
+        with open("high_score.txt", "r") as h:
+            hi = h.read()
 
